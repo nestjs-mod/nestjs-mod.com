41ef186494f75604489977e96ffdbe41f3c6f2b4
EndyKaufman <admin@site15.ru>
2024-08-05 15:58:50 +0400

    fix: remove build from gitignore

---
+++ build/assets/js/5e95c892.9ebacf52.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknestjs_mod_com=self.webpackChunknestjs_mod_com||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>d});r(6540);var c=r(4164),n=r(1003),t=r(7559),u=r(2831),a=r(781),o=r(4848);function d(e){return(0,o.jsx)(n.e3,{className:(0,c.A)(t.G.wrapper.docsPages),children:(0,o.jsx)(a.A,{children:(0,u.v)(e.route.routes)})})}}}]);
